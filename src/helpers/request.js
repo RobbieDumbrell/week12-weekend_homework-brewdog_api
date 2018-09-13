08eb284b6592426984879276899fdb1c6197726d
Robbie Dumbrell <robert.dumbrell@outlook.com>
2018-09-13 19:31:19 +0100

    project set up and helpers added

---
+++ src/helpers/request.js
@@ -0,0 +1,15 @@
+const RequestHelper = function (url) {
+    this.url = url
+  }
+  
+  RequestHelper.prototype.get = function () {
+    return fetch(this.url, {
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+      .then(response => response.json());
+  };
+  
+  module.exports = RequestHelper;
+  
